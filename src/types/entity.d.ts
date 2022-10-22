ba5746e0686bd1f46cc2bf21a8316513590d11fc
zowie <zowietao@139.com>
2022-10-22 16:44:17 +0800

    rename type and sort

---
+++ src/types/entity.d.ts
@@ -0,0 +1,41 @@
+import { RequestFunc } from '@/constant';
+
+type WsResponse = {
+  event: RequestFunc;
+  errCode: number;
+  errMsg: string;
+  data: any;
+  operationID: string;
+};
+
+type IMConfig = {
+  platform: number;
+  api_addr: string;
+  ws_addr: string;
+  log_level: number;
+};
+
+type OfflinePush = {
+  title: string;
+  desc: string;
+  ex: string;
+  iOSPushSound: string;
+  iOSBadgeCount: boolean;
+};
+
+type MessageEntity = {
+  type: string;
+  offset: number;
+  length: number;
+  url?: string;
+  info?: string;
+};
+
+type PicBaseInfo = {
+  uuid: string;
+  type: string;
+  size: number;
+  width: number;
+  height: number;
+  url: string;
+};
