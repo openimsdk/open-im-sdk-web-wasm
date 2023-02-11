dbe42bd7689896e56668b3c50ddde8b0c62ba3e7
zowie <zowietao@gmail.com>
2023-02-11 17:46:45 +0800

    remove ignore

--- src/sqls/localChatLogReactionExtensions.ts
+++ src/sqls/localChatLogReactionExtensions.ts
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/no-unsafe-call */
 import squel from 'squel';
 import { Database, QueryExecResult } from '@jlongster/sql.js';
 import { jsonDecode, jsonEncode } from '@/utils';
