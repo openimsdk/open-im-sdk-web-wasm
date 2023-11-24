6e983e74ce03cb6512d06f12206426e5b1137d7c
Bloomingg <1293499952@qq.com>
2023-11-24 16:26:27 +0800

    fix: createImageMessage params

--- src/sdk/index.ts
+++ src/sdk/index.ts
@@ -391,6 +391,7 @@ class SDK extends Emitter {
       window.createImageMessageByURL,
       [
         operationID,
+        params.sourcePath,
         JSON.stringify(params.sourcePicture),
         JSON.stringify(params.bigPicture),
         JSON.stringify(params.snapshotPicture),
