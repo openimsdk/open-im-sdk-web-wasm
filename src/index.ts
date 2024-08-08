dedfaa3483e12ae1424297c9aebdc4c36c0a1bb2
卡色 <cipchk@qq.com>
2024-08-08 13:47:24 +0800

    fix: correction type export
    - close #92

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,6 @@ export { getSDK } from './sdk';
 
 export { CbEvents } from './constant';
 export * from './types/enum';
+export * from './types/entity';
+export * from './types/eventData';
+export * from './types/params';
